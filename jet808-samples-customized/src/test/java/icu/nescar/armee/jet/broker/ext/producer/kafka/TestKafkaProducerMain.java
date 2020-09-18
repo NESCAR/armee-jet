b79eea4c1fe9bd8822a5341db3bdf4ba1b18be5e
neyzoter <sonechaochao@gmail.com>
2020-09-18 15:56:23 +0800

    add time converter

--- jet808-samples-customized/src/test/java/icu/nescar/armee/jet/broker/ext/producer/kafka/TestKafkaProducerMain.java
+++ jet808-samples-customized/src/test/java/icu/nescar/armee/jet/broker/ext/producer/kafka/TestKafkaProducerMain.java
@@ -5,6 +5,7 @@ import icu.nescar.armee.jet.broker.ext.conf.ConfArguments;
 import icu.nescar.armee.jet.broker.ext.producer.Producer;
 import icu.nescar.armee.jet.broker.ext.producer.kafka.msg.KafkaMsgKey;
 import icu.nescar.armee.jet.broker.msg.req.AlarmUploadRequestMsgBody;
+import icu.nescar.armee.jet.broker.util.TimeConverter;
 
 /**
  * Main函数发送Kafka消息
@@ -14,15 +15,19 @@ public class TestKafkaProducerMain {
     public static void main(String[] args){
         Producer<KafkaMsgKey, Object> implSync = new KafkaProducerImpl<>(ConfArguments.KAFKA_TOPIC_DATA, false);
         Producer<KafkaMsgKey, Object> implAsync = new KafkaProducerImpl<>(ConfArguments.KAFKA_TOPIC_DATA, true);
+        int step = 0;
         while (true) {
             try {
                 Thread.sleep(5000);
                 KafkaMsgKey key = new KafkaMsgKey("client1", Jt808MsgType.CLIENT_ALARM_INFO_UPLOAD.getMsgId());
                 AlarmUploadRequestMsgBody msg = new AlarmUploadRequestMsgBody();
-                msg.setAlarmStatus((byte)1);
-                msg.setTime("2019-08-09T12:03:02Z");
+                msg.setAlarmStatus((byte) (1 << step));
+                msg.setTime(TimeConverter.longToRcf3339(System.currentTimeMillis()));
+                // 异步测试
                 implAsync.send(key, msg);
+                // 同步测试
 //                implSync.send(key, msg);
+                step = (step + 1) % 8;
             } catch (Exception e) {
                 e.printStackTrace();
             }
