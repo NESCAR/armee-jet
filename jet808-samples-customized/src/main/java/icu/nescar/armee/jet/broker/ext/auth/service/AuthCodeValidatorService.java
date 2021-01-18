be21c26714aad779328a77dce2f9d15cf989cc73
whale <hujing@whaledeMacBook-Pro.local>
2021-01-18 11:01:30 +0800

    fix

--- jet808-samples-customized/src/main/java/icu/nescar/armee/jet/broker/ext/auth/service/AuthCodeValidatorService.java
+++
@@ -1,7 +0,0 @@
-package icu.nescar.armee.jet.broker.ext.auth.service;
-
-import io.github.hylexus.jt808.ext.AuthCodeValidator;
-
-public interface AuthCodeValidatorService extends AuthCodeValidator {
-
-}
